6d9420337c9fe7da36db6fd0941d762921ca7f0f
Audrey Billoteau <abillote@c4a11c10.42berlin.de>
2025-07-18 17:01:02 +0200

    ex01 done

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 13:40:11 by abillote          #+#    #+#             */
-/*   Updated: 2025/07/18 14:50:25 by abillote         ###   ########.fr       */
+/*   Updated: 2025/07/18 16:50:47 by abillote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,13 +36,41 @@ Fixed::~Fixed()
 	std::cout << "Destructor called\n";
 }
 
+Fixed::Fixed(const int input)
+{
+	std::cout << "Int constructor called\n";
+	m_fp_value = input << m_fractionalBits;
+}
+
+Fixed::Fixed(const float input)
+{
+	std::cout << "Float constructor called\n";
+	m_fp_value = (int)roundf(input * (1 << m_fractionalBits));
+}
+
+float Fixed::toFloat(void) const
+{
+	return (float)m_fp_value / (1 << m_fractionalBits);
+}
+
+int Fixed::toInt(void) const
+{
+	return (int)m_fp_value >> m_fractionalBits;
+}
+
+
 int Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called\n";
 	return m_fp_value;
 }
 
 void Fixed::setRawBits(int const raw)
 {
 	m_fp_value = raw;
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed)
+{
+	out << fixed.toFloat();
+	return out;
 }
---
+++ CPP02/ex01/Fixed.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 13:40:22 by abillote          #+#    #+#             */
+/*   Updated: 2025/07/18 15:52:19 by abillote         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed(const int input);
+		Fixed(const float input);
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		~Fixed();
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
+
+	private:
+		int m_fp_value;
+		static const int m_fractionalBits = 8;
+};
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed);
+
+#endif
