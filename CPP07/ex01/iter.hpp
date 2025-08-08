115c15afa20b886b244063919e5d8a9e25fe4555
AudreyBil <15158467+AudreyBil@users.noreply.github.com>
2025-08-08 15:48:58 +0200

    CPP07 ex01

---
+++ CPP07/ex01/iter.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/08 11:10:55 by abillote          #+#    #+#             */
+/*   Updated: 2025/08/08 15:48:14 by abillote         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+#include <cctype>
+
+template <typename ArrayType, typename SizeType, typename FunctionType>
+void iter(ArrayType* array, SizeType length, FunctionType func)
+{
+	if (array == NULL)
+		return;
+	for (SizeType i = 0; i < length; i++) {
+		func(array[i]);
+	}
+}
+
+template <typename ArrayType, typename SizeType, typename FunctionType>
+void iter(const ArrayType* array, SizeType length, FunctionType func)
+{
+	if (array == NULL)
+		return;
+	for (SizeType i = 0; i < length; i++) {
+		func(array[i]);
+	}
+}
+
+template <typename T>
+void printConst(const T& element)
+{
+	std::cout << "Printing the array element (const): " << element << std::endl;
+}
+template <typename T>
+void printNonConst(T& element)
+{
+	std::cout << "Printing the array element (non-const):: " << element << std::endl;
+}
+
+
+
+#endif
---
+++ CPP07/ex01/main.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/08 11:13:58 by abillote          #+#    #+#             */
+/*   Updated: 2025/08/08 15:42:37 by abillote         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iter.hpp"
+
+#define CYAN	"\033[36m"
+#define BOLD	"\033[1m"
+#define RESET	"\033[0m"
+
+void increment(int& nb)
+{
+	nb++;
+	std::cout << "Printing the array element after incrementation: " << nb << std::endl;
+}
+
+int main( void ) {
+
+	{
+	std::cout << CYAN << BOLD << "--- Test integers array with an instantiated function template---" << RESET << std::endl;
+	int intArray[3] = {1, 2, 3};
+	::iter(intArray, 3, printNonConst<int>);
+
+	const int intArrayConst[3] = {4, 5, 6};
+	::iter(intArrayConst, 3, printConst<int>);
+	}
+	{
+	std::cout << CYAN << BOLD << "\n---Test integers array with a function---" << RESET << std::endl;
+	int intArray[3] = {1, 2, 3};
+	::iter(intArray, 3, increment);
+	}
+	{
+	std::cout << CYAN << BOLD << "\n--- Test char array  with an instantiated function template---" << RESET << std::endl;
+	char charArray[3] = {'a', 'b', 'c'};
+	::iter(charArray, 3, printNonConst<char>);
+
+	const char charArrayConst[3] = {'d', 'e', 'f'};
+	::iter(charArrayConst, 3, printConst<char>);
+	}
+	{
+	std::cout << CYAN << BOLD << "\n--- Test string array---" << RESET << std::endl;
+	char modifiableString[] = "Hello";
+	::iter(modifiableString, 3, printNonConst<char>);
+
+	const char *constString = "Hey";
+	::iter(constString, 3, printConst<char>);
+	}
+
+	{
+	std::cout << CYAN << BOLD << "\n--- Test empty array ---" << RESET << std::endl;
+	int* emptyArray = nullptr;
+	::iter(emptyArray, 0, printConst<int>);
+}
+
+	return 0;
+}
