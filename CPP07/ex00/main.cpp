c4a3ff7ebd5c1cdd824cca2d558e00caf67539cf
AudreyBil <15158467+AudreyBil@users.noreply.github.com>
2025-08-08 13:29:40 +0200

    CPP07 ex00

---
+++ CPP07/ex00/main.cpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/08 11:13:58 by abillote          #+#    #+#             */
+/*   Updated: 2025/08/08 13:27:34 by abillote         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main( void ) {
+
+	std::cout << "---Test integers---\n";
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
+	std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
+
+	std::cout << "\n---Test strings---\n";
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
+	std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
+
+	std::cout << "\n---Test equal values---\n";
+	int x = 5;
+	int y = 5;
+	std::cout << "x = " << x << ", y = " << y << std::endl;
+	std::cout << "min(x, y) = " << ::min(x, y) << std::endl;
+	std::cout << "max(x, y) = " << ::max(x, y) << std::endl;
+
+	std::cout << "\n---Test floats---\n";
+	float f1 = 3.14f;
+	float f2 = 2.71f;
+	::swap(f1, f2);
+	std::cout << "f1 = " << f1 << ", f2 = " << f2 << std::endl;
+	std::cout << "min(f1, f2) = " << ::min(f1, f2) << std::endl;
+	std::cout << "max(f1, f2) = " << ::max(f1, f2) << std::endl;
+
+	std::cout << "\n---Test chars---\n";
+	char ch1 = 'a';
+	char ch2 = 'z';
+	::swap(ch1, ch2);
+	std::cout << "ch1 = " << ch1 << ", ch2 = " << ch2 << std::endl;
+	std::cout << "min(ch1, ch2) = " << ::min(ch1, ch2) << std::endl;
+	std::cout << "max(ch1, ch2) = " << ::max(ch1, ch2) << std::endl;
+
+	return 0;
+}
---
+++ CPP07/ex00/whatever.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/08 11:10:55 by abillote          #+#    #+#             */
+/*   Updated: 2025/08/08 13:26:53 by abillote         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T& arg1, T& arg2)
+{
+	T temp;
+	temp = arg1;
+	arg1 = arg2;
+	arg2 = temp;
+}
+
+template <typename T>
+T min(const T& arg1, const T& arg2)
+{
+	return arg1 < arg2 ? arg1 : arg2;
+}
+
+template <typename T>
+T max(const T& arg1, const T& arg2)
+{
+	return arg1 > arg2 ? arg1 : arg2;
+}
+
+#endif
