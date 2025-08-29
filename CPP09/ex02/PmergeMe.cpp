8f8ec70c8132a77bde19fb62dc1f1564da72dffd
AudreyBil <15158467+AudreyBil@users.noreply.github.com>
2025-08-29 15:22:33 +0200

    ex02 beginning

---
+++ CPP09/ex02/PmergeMe.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/26 14:14:39 by abillote          #+#    #+#             */
+/*   Updated: 2025/08/29 15:21:49 by abillote         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe() { };
+
+//PmergeMe::PmergeMe(const PmergeMe& other) { };
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& other)
+{
+	if (this != &other)
+		*this = other;
+	return *this;
+}
+
+PmergeMe::~PmergeMe() { };
+
+std::vector<int> PmergeMe::fordJohnsonVector(std::vector<int> input)
+{
+	(void)input;
+	std::vector<int> sequence;
+	sequence = generateJacobsthalSequence<std::vector<int>>(8);
+	for (int i = 0; i < 8; i++)
+	{
+		std::cout << sequence[i] << std::endl;
+	}
+	return sequence;
+}
+
+int PmergeMe::jacobsthal(int n)
+{
+	if (n == 0)
+		return 0;
+	if (n == 1)
+		return 1;
+	return jacobsthal(n - 1) + 2 * jacobsthal(n - 2);
+}
+
+
+
+
---
+++ CPP09/ex02/PmergeMe.hpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abillote <abillote@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/26 14:14:06 by abillote          #+#    #+#             */
+/*   Updated: 2025/08/29 15:22:00 by abillote         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <cctype>
+#include <cstdlib>
+#include <sstream>
+
+class PmergeMe {
+
+	public:
+		//Constructor
+		PmergeMe();
+		//PmergeMe(const PmergeMe& other);
+		PmergeMe& operator=(const PmergeMe& other);
+		~PmergeMe();
+
+		//Vector implementation
+		std::vector<int> fordJohnsonVector(std::vector<int> input);
+
+		//List implementation
+		//std::list<int> fordJohnsonList(std::list<int> input);
+
+	private:
+		template<typename Container>
+		void printContainer(const Container& container, const std::string& prefix);
+
+		template<typename Container>
+		Container generateJacobsthalSequence(size_t size);
+
+		int jacobsthal(int n);
+
+};
+
+template<typename Container>
+void PmergeMe::printContainer(const Container& container, const std::string& prefix)
+{
+	typename Container::iterator it;
+	std::cout << prefix << std::endl;
+	for (it = container.begin(); it != container.end(); it++) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+}
+
+template<typename Container>
+Container PmergeMe::generateJacobsthalSequence(size_t size)
+{
+	Container sequence;
+	int i = 1;
+	int lastValue = -1;
+
+	while (sequence.size() < size) {
+		int jn = jacobsthal(i);
+		if (jn != 0 && jn != lastValue) {
+			sequence.push_back(jn);
+			lastValue = jn;
+		}
+		i++;
+	}
+
+	return sequence;
+}
+
+
+
+#endif
